9d1f76b525c0f0e7d030800d7b3b8d63ad4ae8f9
Cait Johnson <caitjohnson@wrightsgate.net>
2023-03-24 16:21:20 -0800

    Add files via upload

---
+++ week-2/johnson_myworld.py
@@ -0,0 +1,9 @@
+# ==============================================
+# ; Title: johnson_myworld.py
+# ; Author: Caitlynne Johnson
+# ; Date: 24 March 2023
+# ; Description: First Python Program.
+
+# =============================================
+
+print("You are now in Johnson's world!")
